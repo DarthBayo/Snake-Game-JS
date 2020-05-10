cb4c1968017f40fcc0b415fe69b92fb625c67d52
Giovani <giovaniequisini@gmail.com>
2020-05-09 22:23:43 -0300

    NodeJS to deploy

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const dotenv  = require("dotenv/config");
+
+const app = express();
+
+app.use(express.static(__dirname + "/index.html"));
+app.use(express.static(__dirname));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT);
